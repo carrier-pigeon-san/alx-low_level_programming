9cc8bb72368a1b0ab1af82ac53b9b1dedd7ec396
Sandy Obare <obaresandy@gmail.com>
2023-06-16 00:01:09 +0000

    0x00. C - Hello World project task_#4 update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-
-int main ()
+/*This function prints a simple statement*/
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
 }
 
