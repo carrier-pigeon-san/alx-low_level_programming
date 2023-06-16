4314ff4b60f9902bef78f5a0acaf5b2da8015531
Sandy Obare <obaresandy@gmail.com>
2023-06-16 01:20:50 +0000

    0x00. C - Hello World project task_#8 update1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	const char *msg;
 
-	msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,";
 	write(2, msg, strlen(msg));
 
 	return (1);
