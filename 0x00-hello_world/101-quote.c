f4f3a6cb2a2083268ca7acfad8203fc2b00da6ca
Sandy Obare <obaresandy@gmail.com>
2023-06-16 01:13:28 +0000

    0x00. C - Hello World project task_#8 initial commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char *msg;
+
+	msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19,\n";
+	write(2, msg, strlen(mesg));
+
+	return (1);
+}
+
