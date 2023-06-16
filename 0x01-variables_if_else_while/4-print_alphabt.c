de22b91f2c06e813fe2de846af5b2ecf60394a3c
Sandy Obare <obaresandy@gmail.com>
2023-06-16 23:56:45 +0000

    C - Variables, if, else, while project task_4 initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Prints alphabet in lowercase except for letter e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		if (c != 'e' && 'q')
+			putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
