51308ab24b5acafd66573210ac1053e98ddc29d2
Sandy Obare <obaresandy@gmail.com>
2023-06-17 02:00:47 +0000

    C - Variables, if, else, while project task_8 initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prints all numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 0;
+	while (c < 10)
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'a';
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
