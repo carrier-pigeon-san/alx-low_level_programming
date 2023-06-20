a1b4c3b498f2fdb5c090497585994257b65d89c7
Sandy Obare <obaresandy@gmail.com>
2023-06-20 21:47:26 +0000

    C - Functions, nested loops project initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -  Prints _putchar
+ *
+ * Return: Alwats 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#C - Functions, nested loops
+This repository contains C programs that illustrate functions and nested loops
