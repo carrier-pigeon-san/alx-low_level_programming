5e87208ba007a9526796674e607ccda7500fb692
Sandy Obare <obaresandy@gmail.com>
2023-06-21 13:53:16 +0000

    C - Functions, nested loops project task6 initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute vale of an integer
+ * @int: number whose absolute value is to be determined
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int)
+{
+
+	if (int < 0)
+	{
+		_putchar(int * -1);
+	}
+	else
+	{
+		_putchar(int);
+	}
+
+	return (0);
+}
