c6fd1ad7dca166343edcf335a4be35e24d26e449
Sandy Obare <obaresandy@gmail.com>
2023-06-21 22:34:19 +0000

    C - Functions, nested loops projects task10 initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @int - integer 1
+ * @int - integer 2
+ *
+ * Return: sum of integers
+ */
+int add(int, int);
+int add(int x, int y)
+{
+	return (x + y);
+}
