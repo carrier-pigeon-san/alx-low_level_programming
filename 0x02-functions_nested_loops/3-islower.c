3492de524429f24819962332a8049cc58e0ef0a6
Sandy Obare <obaresandy@gmail.com>
2023-06-20 18:39:07 -0700

    C - Functions, nested loops task3[B initial commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _islower - checks if given character is lowercase
+ *
+ * Return: 1 if character is lowercase
+ * 0 if otherwise
+ */
+int _islower(int c);
+{
+	int c;
+
+	if (c >= 'a' && c <= 'z')
+		n = 1
+	else
+		n = 0
+
+	return (n);
+}
