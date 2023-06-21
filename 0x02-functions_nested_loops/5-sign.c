d0da2bbad1d0a33881aa208e39eb0b2c05981a0d
Sandy Obare <obaresandy@gmail.com>
2023-06-21 02:37:59 +0000

    C - Functions, nested loops project task5 initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * prit_sign - prints the sign of a number
+ * @n: number whose sign is to be determined
+ *
+ * Return: 1 if positive
+ * 0 if 0
+ * -1 if negative
+ */
+int print_sign(int n)
+{
+	int sign;
+
+	if (n < 0)
+	{
+		sign = -1;
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('-');
+		_putchar('1');
+	}
+	else if (n == 0)
+	{
+		sign = 0;
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0');
+	}
+	else
+	{
+		 sign = -1;
+		 _putchar('+');
+		 _putchar(',');
+		 _putchar(' ');
+		 _putchar('1');
+	}
+
+	return (sign);
+}
