341619febe614157021731886741b166c6eb3f1d
Sandy Obare <obaresandy@gmail.com>
2023-06-22 18:56:50 +0000

    C - More functions, more nested loops project task2 initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first parameter to be multiplied
+ * @b: second parameter to be multiplied
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
