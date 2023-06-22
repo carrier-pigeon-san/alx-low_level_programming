4ea3b1ee274311a4cc5a2c6b118fe41a1b94a479
Sandy Obare <obaresandy@gmail.com>
2023-06-22 19:27:16 +0000

    C - More functions, more nested loops project task3 initial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0 to 9, followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		_putchar(c + '0');
+	}
+	return (0);
+}
