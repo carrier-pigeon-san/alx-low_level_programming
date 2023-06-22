9fe293dc371b739dc8e19304a524ed0b80ad4cf1
Sandy Obare <obaresandy@gmail.com>
2023-06-22 20:15:29 +0000

    C - More functions, more nested loops project task4 header initial commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9, excluding 2 and 4, followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	c = 0;
+	while (c <= 9)
+	{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
