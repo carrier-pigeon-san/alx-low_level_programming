fe302f50b3aa96939cb81aec23d3b26e3e6164f5
Sandy Obare <obaresandy@gmail.com>
2023-06-23 00:26:34 +0000

    C - More functions, more nested loops project task8 initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_square - draws a square on the terminal
+ * @size: size of the square
+ *
+ * Return: Always 0
+ */
+void print_square(int size)
+{
+	int i;
+	int j:
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < n)
+		{
+			j = 0;
+			while (j < n)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
