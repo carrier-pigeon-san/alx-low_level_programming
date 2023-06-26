b294618a8c2b0df1e46581886adfc459f9b16737
Sandy Obare <obaresandy@gmail.com>
2023-06-26 22:15:44 +0000

    C - Pointers, arrays and strings initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates the value that a pointer points to
+ * @n: value to be updated
+ *
+ * Return:Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
