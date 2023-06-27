c55a6046533d7fe05ac04db0083c00780b6fc720
Sandy Obare <obaresandy@gmail.com>
2023-06-27 21:31:48 +0000

    C - Pointers, arrays and strings project task7 initial commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: String argument
+ *
+ * Return:Always 0
+ */
+
+void puts_half(char *str)
+{
+	int l;
+
+	c = 0;
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	if (l % 2 == 0)
+		l = l / 2;
+	else
+		l = (l - 1) / 2;
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
