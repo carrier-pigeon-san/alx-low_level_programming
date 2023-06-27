2901a1273b92a14ea967572522b6b1403ceb1cc7
Sandy Obare <obaresandy@gmail.com>
2023-06-27 20:26:24 +0000

    C - Pointers, arrays and strings project task4 initial commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
  * print_rev - prints string in reverse followed by a new line
- * _strlen - find length of string argument in _puts function
  * @s: string argument
  *
  * Return:Always 0
  */
-
 int _strlen(char *s);
 
 void print_rev(char *s)
@@ -21,3 +19,23 @@ void print_rev(char *s)
 	} while (c >= 0);
 	_putchar('\n');
 }
+
+/**
+ * _strlen - find length of string argument in _puts function
+ * @s: string argument
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
