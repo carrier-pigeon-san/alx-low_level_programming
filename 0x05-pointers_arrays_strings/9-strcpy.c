39d259c21f5de8ab4087aeb89e8ce6584a9ca3b8
Sandy Obare <obaresandy@gmail.com>
2023-06-27 23:13:21 +0000

    C - Pointers, arrays and strings project task9 initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strcpy - copies a string
+ * @dest: destination
+ * @src: source
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = str;
+	return (*dest);
+}
