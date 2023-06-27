93732da034fb17d4996b844c02cb8551d1ac55de
Sandy Obare <obaresandy@gmail.com>
2023-06-27 20:50:07 +0000

    C - Pointers, arrays and strings project task4 code update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,36 +5,20 @@
  *
  * Return:Always 0
  */
-int _strlen(char *s);
 
 void print_rev(char *s)
 {
 	int c;
 
-	c = _strlen(s);
+	c = 0;
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
 	do {
 		_putchar(s[c]);
 		c--;
 
 	} while (c >= 0);
 }
-
-/**
- * _strlen - find length of string argument in _puts function
- * @s: string argument
- *
- * Return: length of string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
