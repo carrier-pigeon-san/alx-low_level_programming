c1bc893c72459c62fc29dc407859cf4dd448598d
Sandy Obare <obaresandy@gmail.com>
2023-06-28 00:56:54 +0000

    C - Pointers, arrays and strings project task5 update

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,15 +9,19 @@
 void rev_string(char *s)
 {
 	int c;
+	int i;
+	char w;
 
 	c = 0;
 	while (s[c] != '\0')
 		c++;
 	c--;
+	i = 0;
 	while (c >= 0)
 	{
-		_putchar(s[c]);
-		c--;
+		w = s[c];
+		s[c] = s[i];
+		s[i] = w;
+		
 	}
-	_putchar('\n');
 }
