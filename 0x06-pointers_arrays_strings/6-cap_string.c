d5b6e33682f82dd8fd2056f33f811e1dbc3d2ea6
Sandy Obare <obaresandy@gmail.com>
2023-07-01 18:05:00 +0000

    C - More pointers, arrays and strings project task6 initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes first letter of each word after specific characters
+ * @str: string argument
+ *
+ * Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int c, n, s;
+	char sym[] = ",;.!?(){}\n\t\" ";
+
+	c = 0;
+	n = c - 1;
+	while (str[c] != '\0')
+	{
+		if (str[c] > 96 && str[c] < 123)
+		{
+			s = 0;
+			while (sym[s] != '\0')
+			{
+				if (sym[s] == str[n])
+				{
+					if (str[c] > 96 && str[c] < 123)
+						str[c] -= 32;
+				}
+				s++;
+			}
+		}
+		c++;
+		n++;
+	}
+	return (str);
+}
