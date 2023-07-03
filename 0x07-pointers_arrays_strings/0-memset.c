79c2a9d464527fd27ca4784e30ae1b0d9aadc9d9
Sandy Obare <obaresandy@gmail.com>
2023-07-03 22:02:03 +0000

    0x07-pointers_strings_arrays project task0 initial commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes
+ *
+ * Return: memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int c, i;
+
+	for (c = 0; s[c] != '\0'; c++)
+		;
+
+	for (c--, i = 0; i <= n; i++, c++)
+		s[c] = b;
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+#C - Pointers, arrays and strings
+This repository directory contains C programs based on pointers, arrays and strings.
