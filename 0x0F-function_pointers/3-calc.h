640b7d394f581a72388cb78e83385fd44c1ae5c1
Sandy Obare <obaresandy@gmail.com>
2023-07-22 12:24:55 +0000

    0x0F-pointer_functions projects task #3 code update

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -19,7 +19,7 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int)i;
+int (*get_op_func(char *s))(int, int);
 
 
 #endif /* FUNCTION_POINTERS_H */
