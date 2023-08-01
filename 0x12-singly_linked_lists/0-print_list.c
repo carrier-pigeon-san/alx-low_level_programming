e34d35a0852229fd1f43a592011611887d0d1e48
Sandy Obare <obaresandy@gmail.com>
2023-08-01 21:06:09 +0000

    0x12-singly_linked_lists project task #0 initial commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list
+ * @h: head of the list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	list_t *temp = h;
+	int nod = 0;
+
+	while (temp != NULL)
+	{
+		if (new->str == NULL)
+			printf("[%d] %s", 0, "(ni;)");
+		else
+			printf("[%d] %s", new->len, new->str);
+		nod++;
+	}
+	return (nod);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,2 @@
+#0x12. C - Singly linked lists
+This directory contains solutions for singly linked lists project tasks implemented in C programming language.
