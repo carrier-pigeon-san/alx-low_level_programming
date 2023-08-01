10ab12416d86ae6b7e3fa9346c0427648c156736
Sandy Obare <obaresandy@gmail.com>
2023-08-01 22:59:31 +0000

    0x12-singly_linked_lists project task #3 initial commit

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * add_node_end - adds a new node at the end of a list
+ * @head: head of the list
+ * @str: node data
+ * Return: pointer to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int l = 0;
+	list_t *nod;
+
+	while (str[l] != '\0')
+		l++;
+
+	nod = malloc(sizeof(list_t));
+	if (nod == NULL)
+	{
+		free(nod);
+		return (NULL);
+	}
+
+	nod->len = l;
+	nod->str = strdup(str);
+	nod->next = NULL;
+
+	*head = nod;
+	return (nod);
+}
