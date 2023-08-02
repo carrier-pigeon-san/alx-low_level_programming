a077582aff1c08206f464954453701bef9523601
Sandy Obare <obaresandy@gmail.com>
2023-08-02 13:52:11 +0000

    0x12-singly_linked_lists project task #7 debugging

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @index: index of the node starting at 0
  * Return: nth node of the list or NULL if it does not exist
  */
-listint_t *get_node_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
 	unsigned int i = 0;
