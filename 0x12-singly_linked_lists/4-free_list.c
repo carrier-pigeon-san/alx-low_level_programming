ea4ad63ca61f844273122733b4c97e42b90673a3
Sandy Obare <obaresandy@gmail.com>
2023-08-02 00:18:04 +0000

    0x12-singly_linked_lists project task #4 initial commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees memory unused by a list
+ * @head: head of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		free(temp);
+		temp = temp->next;
+	}
+}
