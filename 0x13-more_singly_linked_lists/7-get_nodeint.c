ecdb22e965697435b3c7dd37e537aefcec0893b1
Sandy Obare <obaresandy@gmail.com>
2023-08-02 13:53:14 +0000

    0x12-singly_linked_lists project task #7 debugging

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_node_at_index - returns the nth node of a list
+ * get_nodeint_at_index - returns the nth node of a list
  * @head: pointer to the head of the list
  * @index: index of the node starting at 0
  * Return: nth node of the list or NULL if it does not exist
