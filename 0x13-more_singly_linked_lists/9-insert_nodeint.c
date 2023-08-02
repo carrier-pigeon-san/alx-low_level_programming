911c691f5ff2a953343eb04965fbf1166e635c53
Sandy Obare <obaresandy@gmail.com>
2023-08-02 19:19:49 +0000

    0x12-singly_linked_lists project task #9 debugging

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 
-	while (c < idx)
+	while (c < idx - 1)
 	{
 		if (temp != NULL)
 			temp = temp->next;
