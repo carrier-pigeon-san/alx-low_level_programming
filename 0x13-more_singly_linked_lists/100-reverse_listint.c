2541a949a634f8cec8d2d7d4166dc0906bd015aa
Sandy Obare <obaresandy@gmail.com>
2023-08-02 21:19:49 +0000

    0x12-singly_linked_lists project task #11 initial commit

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * reverse_list - reverses a list
+ * @head: double pointer to head of the list
+ * Return: pointer to first node of the list
+ */
+listint_t *reverse_listint(list_int_t *head)
+{
+	listint_t *prev;
+	listint_t *next;
+
+	prev = NULL;
+	while (*head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+
+	*head = prev;
+
+	return (*head);
+}
