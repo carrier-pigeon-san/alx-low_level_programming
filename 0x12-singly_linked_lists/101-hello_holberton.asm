ddf6150d0da61ca347e6128834bb7a8f07a96c45
Sandy Obare <obaresandy@gmail.com>
2023-08-02 01:47:39 +0000

    0x12-singly_linked_lists project task #6 code update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,19 +1,19 @@
 section .data
-    hello_string db "Hello, Holberton", 0
+    hello_string db "Hello, Holberton", 10, 0 ; 10 is the newline character
 
 section .text
     global main
-
-extern printf
+    extern printf
 
 main:
-    sub rsp, 8 ; align the stack on a 16-byte boundary
-    mov rdi, hello_string ; load the address of the string to print into rdi
-    call printf ; call the printf function to print the string
-    add rsp, 8 ; restore the stack pointer
+    sub rsp, 8           ; Adjust the stack to align on a 16-byte boundary
+    mov rdi, hello_string ; Load the address of the string to print into rdi
+    xor rax, rax         ; Clear rax register (used to indicate the number of vector registers used)
+    call printf          ; Call the printf function to print the string
+    add rsp, 8           ; Restore the stack pointer
 
     ; Exit the program
-    mov eax, 60        ; syscall: exit
-    xor edi, edi       ; status: 0
-    syscall           ; invoke syscall
+    mov eax, 60          ; syscall: exit
+    xor edi, edi         ; status: 0
+    syscall             ; invoke syscall
 
