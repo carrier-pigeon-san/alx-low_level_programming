d006bf2f860543fb7854d0cee7fbed68f1154849
Sandy Obare <obaresandy@gmail.com>
2023-08-02 18:33:09 +0000

    0x12-singly_linked_lists project task #8 initial commit

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * sum_listint - returns sum of all the data(n) of listint_t linked list
+ * @head:pointer to the head of the linked list
+ *
+ * Return: 0 if the list is empty
+ */
+int sum_listint(listint_t *head)
+{
+	int sum;
+	listint_t *nod;
+
+	if (head != NULL)
+	{
+		nod = head;
+		sum = 0;
+
+		while (nod != NULL)
+		{
+			sum = sum + nod->n;
+			node = node->next;
+		}
+
+
+		return (sum);
+	}
+
+	return (0);
+}
