f89029f1ac571a89be8da7bbb676ea2b7d0fd951
Sandy Obare <obaresandy@gmail.com>
2023-08-30 21:05:20 +0000

    0x08-recursion project task #5 initial commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+int p_square(int i, int v);
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer argument
+ * Return: square root
+ */
+it _sqrt_recursion(int n)
+{
+	int v = 2;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	return (p_square(n, v));
+}
+
+/**
+ * p_square - helper function to determine if n is a perfect square
+ * @i: integer argument
+ * @v: divisor
+ * Return: sqrt
+ */
+int p_square(int i, int v)
+{
+	if (v > i / 2)
+		return (-1);
+	if (n % v == 0 && i / v == v)
+		return (v);
+	return (p_square(i, v + 1));
+}
