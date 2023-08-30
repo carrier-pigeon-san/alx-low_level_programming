6ca6f57b0b00de8ef79146a4ae9b3b7676ca34ef
Sandy Obare <obaresandy@gmail.com>
2023-08-30 19:39:31 +0000

    0x08-recursion project task #1 initial commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - recursive function prints a string in reverse
+ * @s: string argument
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_puts_recursion(s + 1);
+	putchar(*s);
+}
