9ef969faf2e1e0c86095531a14eb2d346141e064
Sandy Obare <obaresandy@gmail.com>
2023-09-01 13:42:13 +0000

    0x0A-argc_argv project initial commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+#0x0A. C - argc, argv
+This directory contains tasks for project #0x0A, regarding arguments to the main function implemented in C programming
