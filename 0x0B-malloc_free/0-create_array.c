b7f16aa770bc2e6f8206d7c57b03b1eb3f37f129
Sandy Obare <obaresandy@gmail.com>
2023-09-05 13:15:09 +0000

    0x0B-malloc_free project, task #0 update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	arr = malloc(sizeof(int) * size);
+	arr = malloc(sizeof(unsigned int) * size);
 
 	if (arr == NULL)
 		return (NULL);
