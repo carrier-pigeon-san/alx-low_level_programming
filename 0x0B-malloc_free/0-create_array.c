241661729040501c7b9e887d53a4dfc6afe3530c
Sandy Obare <obaresandy@gmail.com>
2023-09-05 13:06:24 +0000

    0x0B-malloc_free project initial commit

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars and...
+ * ...initializes it with a specific char
+ * @size: size of array
+ * @c: character array is to be initialized with
+ * Return: pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int) * size);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,2 @@
+#0x0B. C - malloc, free
+This directory contains project #0x0B tasks solutions pertaining to the use of malloc and free, implemented in C programming
