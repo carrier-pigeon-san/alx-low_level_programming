59448d5000085f9f8f2c2b8f423b4b82a3841b2f
Sandy Obare <obaresandy@gmail.com>
2023-09-06 21:21:42 +0000

    0x0A-argc_argv project task #5 initial commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * main - prints minimum number of coins to make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector
+ * Return 0 (Success)
+ */
+int main(int argc, char **argv)
+{
+	int i, bal, num;
+	int coin = 0;
+	int denom[5] = {25, 10, 5, 2, 1};
+
+	if (arg != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	bal = atoi(argv[1]);
+
+	if (bal < 1)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+		num = nal / denom[i];
+		coin += num
+		bal -= num * denom[i];
+	}
+	printf("%d\n", coin);
+	return (0);
+}
