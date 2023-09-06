a484892a9796c7e9eb89898262789fb5bafcb704
Sandy Obare <obaresandy@gmail.com>
2023-09-06 19:51:30 +0000

    0x0B-malloc_free project task #3 initial commit

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @height: rows
+ * @width: columns
+ * Return: pointer to pointer of array
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr, i, j;
+
+	arr = (int **)malloc(height * sizeof(int *));
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+			return (NULL);
+	}
+
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	return (arr);
+}
