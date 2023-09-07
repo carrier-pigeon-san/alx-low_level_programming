b6274f9e87a41fb01a5ad3a2096398ef6705f030
Sandy Obare <obaresandy@gmail.com>
2023-09-07 19:22:54 +0000

    0x0C-more_malloc_free project task #2 initial commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an array of nmemb elements of size bytes...
+ * ...each and returns a pointer to the allocated memory
+ * @nmemb: number of elements
+ * @size: size of each element in bytes
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *mem;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		mem[i] = 0;
+	return (mem);
+}
