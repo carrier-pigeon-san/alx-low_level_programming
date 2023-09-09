efe6b5c2040aa4c588775f45a51011b5a68d086c
Sandy Obare <obaresandy@gmail.com>
2023-09-09 20:50:29 +0000

    0x0C-more_malloc_free project task #4 initial commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ * _realloc -  reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated with a call to malloc
+ * @old_size: the size, in bytes, of the allocated space for ptr
+ * @new_size: the new size, in bytes of the new memory block
+ * Return: void;
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	size_t i;
+	unsigned char *byte_ptr, *byte_sec_ptr;
+	void *sec_ptr;
+
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr != NULL)
+	{
+		ptr = malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size > old_size)
+	{
+		sec_ptr = malloc(new_size);
+		if (sec_ptr == NULL)
+			return (NULL);
+		byte_ptr = (unsigned char *)ptr;
+		byte_sec_ptr = (unsigned char *)sec_ptr;
+		for (i = 0; i < old_size; i++)
+			byte_sec_ptr[i] = byte_ptr[i];
+		free(ptr);
+		return (sec_ptr);
+	}
+	sec_ptr = malloc(new_size);
+	if (sec_ptr == NULL)
+		return (NULL);
+	byte_ptr = (unsigned char *)ptr;
+	byte_sec_ptr = (unsigned char *)sec_ptr;
+	for (i = 0; i < new_size; i++)
+		byte_sec_ptr[i] = byte_ptr[i];
+	free(ptr);
+	return (sec_ptr);
+}
