a1cf2d5ef0952150445c72ac918d5964d5eb1b68
Sandy Obare <obaresandy@gmail.com>
2023-09-11 21:22:23 +0000

    0x0E-structures_typedef project initial commit

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+#0x0E. C - Structures, typedef
+This directory contains project #0x0E tasks implementing structures and typdef in C programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - attributes of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: he who let the dogs out
+ *
+ * Description: this struct defines various elements attributed to the...
+ * ...identity of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
