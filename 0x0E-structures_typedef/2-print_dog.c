e8b7c60bad2abd055663b252b110ab77033a35a4
Sandy Obare <obaresandy@gmail.com>
2023-09-11 22:19:12 +0000

    0x0E-structures_typedef project task #2 initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * print_dog - prints a struct of type dog
+ * @d: struct argument variable
+ * Return: void
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
+		printf("Age: %f\n", (*d).age ? (*d).age : 0);
+		printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
+	}
+}
+
