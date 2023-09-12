96e45cb522904906546258c0c2e8950e4b6ccc60
Sandy Obare <obaresandy@gmail.com>
2023-09-12 00:23:55 +0000

    0x0D-structures_typedef project task#4 fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,8 @@
 #define _DOG_H_
 
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - attributes of a dog
  * @name: name of dog
@@ -20,5 +21,6 @@ typedef struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif /* #ifndef _DOG_H_ */
