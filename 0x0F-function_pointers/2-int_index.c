3a77680ad0fbebe147b029932616e8855aa53f99
Sandy Obare <obaresandy@gmail.com>
2023-09-13 21:47:40 +0000

    0x0F-function_pointers project task #2 fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,15 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size < 0)
+	if (size < 0 || array == NULL || cmp == NULL)
 		return (-1);
 
-	if (array != NULL && cmp != NULL)
-	{
-		for (i = 0; i < size; i++)
-			if (cmp(array[i]) == 1)
-				return (i);
-		return (-1);
-	}
-	return (0);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]) == 1)
+			return (i);
+	return (-1);
 }
