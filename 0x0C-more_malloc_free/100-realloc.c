1e0e7add47db17d0e51bae2a178fc4eb9f93cd5a
Sandy Obare <obaresandy@gmail.com>
2023-09-19 11:31:31 +0000

    0x0C-more_malloc_free project task #4 fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,7 +14,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (ptr != NULL)
+	if (ptr == NULL)i
 	{
 		ptr = malloc(new_size);
 		if (ptr == NULL)
