da8c170aa6fff96d5ccf818e58f3d8e7195d1f64
Sandy Obare <obaresandy@gmail.com>
2023-09-29 15:51:48 +0000

    task #0 initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: unsigned int/0
+ */
+int _pow(int x, int y);
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int dec;
+	int len, i;
+
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len] != '\0'; len++)
+		if (b[len] !='0' && b[len] != '1')
+			return (0);
+	len--;
+	for (i = dec = 0; len >= 0; len --, i++)
+		if (b[len] == '1')
+			dec += _pow(2, i);
+	return (dec);
+}
+/**
+ * _pow_recursion - returns the value of x raised to power y
+ * @x: integer argument
+ * @y: exponential
+ *
+ * Return: exponential product
+ */
+int _pow(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y < 1)
+		return (1);
+	return (x * _pow(x, (y - 1)));
+}
