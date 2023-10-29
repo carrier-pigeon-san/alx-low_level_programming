1fe81f4b2d315e1cf74f0e72af3a69df92e1e06a
Sandy Obare <obaresandy@gmail.com>
2023-10-29 19:11:24 +0000

    3-set_bit.c, 5-flip_bits.c bug-fix2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,7 +9,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int index, notSaime;
+	unsigned int index, notSame;
 
 	for (index = 0, notSame = 0; index < sizeof(n) * 8; index++)
 		if (((n >> index) & 1) != ((m >> index) & 1))
