fd83ba2aeae1a0d084d04d27e3214435e10507e6
Sandy Obare <obaresandy@gmail.com>
2023-10-30 11:18:06 +0000

    directory initialization

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * read_texfile - reads a text file and prints it to the POSIX standard output
+ * @filename: literaly, the file name
+ * @letters: number of letters that should be read and printed
+ *
+ * Return: number of letters read and printed | 0 (failed)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buildStr;
+	ssize_t bytesRD, bytesWR;
+	int sourceFile = open(filename, O_RDONLY);
+
+	if (filename == NULL)
+		return (0);
+
+	if (sourceFile == -1)
+		return (0);
+
+	buildStr = malloc(sizeof(char) * letters);
+	if (buildStr == NULL)
+		return (0);
+
+	bytesRD = read(sourceFile, buildStr, letters);
+	if (bytesRD == -1)
+	{
+		free(buildStr);
+		close(sourceFile);
+		return (0);
+	}
+
+	bytesWR = write(STDOUT_FILENO, buildStr, bytesRD);
+	if (bytesWR != bytesRD)
+	{
+		free(buildStr);
+		close(sourceFile);
+		return (0);
+	}
+
+	free(buildStr);
+	close(sourceFile);
+	return (bytesWR);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+# 0x15. C - File I/O
+This directory contains C programs thet illustrate file i/o operations in `C`
+such as creation, opening, reading, writing and closing using an assortment of
+system calls and functions.
