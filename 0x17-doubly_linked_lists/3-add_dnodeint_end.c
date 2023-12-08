71a78fc572b081e0132ea2c80468f20b912f6687
Sandy Obare <obaresandy@gmail.com>
2023-12-08 21:59:00 +0000

    3-add_dnodeint_end.c line #15 parentheses added to return statement

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
-		return NULL;
+		return (NULL);
 	newnode->n = n;
 	newnode->next = NULL;
 	newnode->prev = NULL;
