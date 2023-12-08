db16359bc436debe0c6b8da44274af37f92866d6
Sandy Obare <obaresandy@gmail.com>
2023-12-08 00:16:34 +0000

    2-add_dnodeint.c line #15 paraentheses on return statement

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	newnode = malloc(sizeof(dlistint_t));
 	if (newnode == NULL)
-		return NULL;
+		return (NULL);
 
 	newnode->n = n;
 	newnode->next = *head;
