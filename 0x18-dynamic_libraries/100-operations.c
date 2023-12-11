51d0f4aa48d89f1f798e772ae5948f320340880a
Sandy Obare <obaresandy@gmail.com>
2023-12-11 19:57:18 +0000

    100-operations.so initial commit

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,57 @@
+#include "main.h"
+/**
+ * add - adds two given numbers
+ * @a: first number
+ * @b: Secod number
+ *
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - substracts one number from another
+ * @a: first number
+ * @b: Secod number
+ *
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplies two given numbers
+ * @a: first number
+ * @b: Secod number
+ *
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - divides two given numbers
+ * @a: first number
+ * @b: Secod number
+ *
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - returns remainder of division between two numbers
+ * @a: first number
+ * @b: Secod number
+ *
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
