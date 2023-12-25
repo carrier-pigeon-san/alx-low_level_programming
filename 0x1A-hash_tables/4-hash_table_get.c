c8ee65e8acdfe646d9079a09ca6d72a69d331aa7
Sandy Obare <obaresandy@gmail.com>
2023-12-25 01:19:42 +0000

    hash_table_get function case: existing key from hash table with collisions

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,6 +9,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *probe;
 
 	if (ht == NULL || key == NULL)
 		return (NULL);
@@ -19,6 +20,16 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(ht->array[index]->key, key) == 0)
 			return (ht->array[index]->value);
+		else
+		{
+			probe = ht->array[index];
+			while (probe != NULL)
+			{
+				probe = probe->next;
+				if (strcmp(probe->key, key) == 0)
+					return (probe->value);
+			}
+		}
 	}
 	return (NULL);
 }
