3a3dc83b02afb4de7e161b317174e366a049232c
Sandy Obare <obaresandy@gmail.com>
2023-12-25 00:23:28 +0000

    case: if hash table is NULL

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -19,7 +19,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	rec->value = strdup(value);
 	rec->next = NULL;
 
-	if (key == NULL || strlen(key) == 0)
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
 	{
 		free(rec);
 		return (0);
