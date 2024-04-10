2b708bbf60bf6d3cf569f8f12addc20b74bf2eab
Sandy Obare <obaresandy@gmail.com>
2024-04-11 02:33:15 +0300

    0-linear.c output message format modified

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
 	{
 		for (index = 0; index < size; index++, *array++)
 		{
-			printf("Value checked array[%ld] = %d\n", index, *array);
+			printf("Value checked array[%ld] = [%d]\n", index, *array);
 			if (*array == value)
 				return (index);
 		}
