578e715518d48c50372676c6aa69d403679adca5
Sandy Obare <obaresandy@gmail.com>
2024-04-26 00:55:40 +0300

    102-print_comb5.c initial commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of two-digit numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit1, digit2, digit3, digit4;
+
+	digit4 = 48;
+	while (digit4 <= 57)
+	{
+		digit3 = 48;
+		while (digit3 <= 57)
+		{
+			digit2 = 48;
+			while (digit2 <= 57)
+			{
+				digit1 = 49;
+				while (digit1 <= 57)
+				{
+					if (digit2 >= digit4 && digit1 > digit3)
+					{
+						putchar(digit4);
+						putchar(digit3);
+						putchar(' ');
+						putchar(digit2);
+						putchar(digit1);
+						putchar(',');
+						putchar(' ');
+					}
+					digit1++;
+				}
+
+				digit2++;
+			}
+
+			digit3++;
+		}
+
+		digit4++;
+	}
+
+	return (0);
+}
