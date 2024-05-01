86b439d545836de91ddecfed99a1fae6f0f84738
Sandy Obare <obaresandy@gmail.com>
2024-05-01 14:16:56 +0300

    9-fizz_buzz.c code update (8)

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,11 +12,11 @@ int main(void)
 	while (n <= 100)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("Fizz");
-		else if (n % 3 == 0)
-			printf("Buzz");
-		else if (n % 5 == 0)
 			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
 		else
 			printf("%d", n);
 		if (n != 100)
