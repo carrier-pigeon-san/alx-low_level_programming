4b3d6a8b36b323042c614b154b0c0a1c38ab3bfd
Sandy Obare <obaresandy@gmail.com>
2024-06-07 23:16:54 +0300

    clearing month #7 project directories: 0x1E-search_algorithms/

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+*/
+void print_number(int n)
+{
+    (void)n;
+    _putchar('9');
+    _putchar('\n');
+}
+int main(void)
+{
+    print_number(20);
+    return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void print_square(int size);
 void print_triangle(int size);
 void print_numbers(void);
 int _putchar(char c);
+void print_number(int n);
